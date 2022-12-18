472ecf925f0121dfee19dbade7f03181ded727f7
Demétrio <dema.atra1@gmail.com>
2022-12-17 21:07:50 -0300

    v. 1.0.0 - HTML

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.listen(8080, () => {
-    console.log('Aplicação rodando com sucesso!')
-})
+app.listen(8080, () => {console.log('Aplicação iniciada com sucesso!')})
 
 app.get('/', (req, res) => {
+    res.status(200).render('index')
+})
 
+app.get('/:x', (req, res) => {
+
+    var pergunta = 'O que é'
+    var descricao = 'Detalhamento da pergunta'
+
+    res.status(200).render('index', {
+        pergunta: pergunta,
+        descricao: descricao 
+    })
 })
