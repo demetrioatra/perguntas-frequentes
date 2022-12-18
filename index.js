44150c44befb119f6057129f99d36b5584b5da8b
Demétrio <dema.atra1@gmail.com>
2022-12-18 20:31:36 -0300

    v. 1.0.3 - Recuperando dados

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const bodyParser = require('body-parser')
 const express = require('express')
-
 const app = express()
 
+//--------------------------------------------------------//
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.listen(8080, () => {console.log('Aplicação iniciada com sucesso!')})
+
+//------------------------ Rotas -------------------------//
 
 app.get('/', (req, res) => {
     res.status(200).render('index')
@@ -12,4 +18,9 @@ app.get('/', (req, res) => {
 
 app.get('/pergunta', (req, res) => {
     res.status(200).render('pergunta')
+})
+
+app.post('/salvar', (req, res) => {
+    var titulo = req.body.titulo;
+    res.status(200).send('Pergunta "' + titulo + '" salva com sucesso!')
 })
